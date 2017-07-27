4a6b568f89d2549ba58606aded5ba32dd0680d77
chakabaka1027 <alexanderdcha@gmail.com>
2017-07-26 20:53:08 -0400

    multiplayer working

---
+++ Assets/Scripts/SetUpLocalPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class SetUpLocalPlayer : NetworkBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		if(isLocalPlayer) {
+            GetComponent<PlayerController>().enabled = true;
+            GetComponent<GravityShift>().enabled = true;
+            transform.Find("MainCamera").GetComponent<Camera>().enabled = true;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+	}
+}
